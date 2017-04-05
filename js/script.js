378503124f1a3d9ac23292f3424ecff1d8660918
Elyriand <steeve.w01@gmail.com>
2017-04-05 10:21:02 +0200

    Ajout détection joueur et etat pion
    Attention : Boucle while infini, l'enlevé pour que ça fonctionne correctement

--- js/script.js
+++ js/script.js
@@ -49,21 +49,64 @@ for(let i=0;i<10;i++){
 var i=true;
 var coordonnée_p1;
 var coordonnée_p2;
+var joueur_actuel=true; //joueur1
 
 function clickdown(){
-	if(i==true){
-		//coordonnée_p1=[];
-		var result = this.id.split(" ");
-		coordonnée_p1=[result[1],result[3]]
-		console.log(coordonnée_p1);
-		console.log("Veuilliez sélectionner une autre case pour effectuer le déplacement");
+	var v_case;
+	var valide=false;
+	if(joueur_actuel==true){
+		v_case=1;
+	}else{
+		v_case=2;
 	}
+	if(i==true){
+		while(valide==false){
+			//console.log("Sélectionner un de vos pions");
+			//coordonnée_p1=[];
+			var result = this.id.split(" ");
+			//if(Board[result[1]+result[3]*10])
+				//console.log(Board[result[1]+result[3]*10]);
+			console.log(Board[(result[1]-1)+(result[3]-1)*10]);
+			
+			if(Board[(result[1]-1)+(result[3]-1)*10]==v_case){
+				console.log("Vous avez bien sélectionner un de vos pions");
+				coordonnée_p1=[result[1]-1,result[3]-1];
+				console.log(coordonnée_p1);
+				console.log("Veuillez sélectionner une autre case pour effectuer le déplacement");
+				valide=true;
+			}else{
+				console.log("Veuillez sélectionner un de vos pions !");
+			}
+		}
+		
+	}
+	valide=false;
 	if(i==false){
-		var result = this.id.split(" ");
-		coordonnée_p2=[result[1],result[3]]
-		console.log(coordonnée_p2);
-		//console.log(result);
-		console.log("Déplacement effectué");
+		while(valide==false){
+			//console.log("Sélectionner un de vos pions");
+			var result = this.id.split(" ");
+			console.log(Board[(result[1]-1)+(result[3]-1)*10]);
+			
+			if(Board[(result[1]-1)+(result[3]-1)*10]==v_case){
+				console.log("Vous avez bien sélectionner un de vos pions");
+				coordonnée_p2=[result[1]-1,result[3]-1];
+				console.log(coordonnée_p2);
+				//console.log(result);
+				console.log("Déplacement effectué");
+				valide=true;
+			}else{
+				console.log("Veuillez sélectionner un de vos pions !");
+			}
+		}	
+			
+
+		joueur_actuel=!joueur_actuel;
+		if(joueur_actuel==true){
+			console.log("Joueur1, à vous de jouer");
+		}else{
+			console.log("Joueur2, à vous de jouer");
+		}
+		
 	}
 	i=!i;
 	console.log(this.id);
