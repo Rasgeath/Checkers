55e450c5faf09717353284e4e87ebe1b5cbf253c
Elyriand <steeve.w01@gmail.com>
2017-04-05 09:53:43 +0200

    Ajout eventlistener + ajout deplacement p1 p2

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var content="<table border='1'>";
 for(let i=0;i<10;i++){
     content+="<tr>";
     for(let j=0;j<10;j++){
-        content+="<td id='cel-c"+(j+1)+"-l"+(i+1)+"' class='";
+        content+="<td id='cel-c "+(j+1)+" -l "+(i+1)+"' class='";
         if((i+j)%2==0) {
             content += "black'></td>";
         }else{
@@ -33,10 +33,49 @@ document.getElementById("echiquier").innerHTML+=content;
 for(let i=0;i<10;i++){
     for(let j=0;j<10;j++) {
         if(Board[i+j*10]==1) {
-            document.getElementById("cel-c" + (i + 1) + "-l" + (j + 1)).innerHTML += ("<img src='img/bfairy.PNG' id='" + (i+10*j) + "'>");
+            document.getElementById("cel-c " + (i + 1) + " -l " + (j + 1)).innerHTML += ("<img src='img/bfairy.PNG' id='" + (i+10*j) + "'>");
         }
         if(Board[i+j*10]==2){
-            document.getElementById("cel-c" + (i + 1) + "-l" + (j + 1)).innerHTML += ("<img src='img/wfairy.PNG' id='" + (i+10*j) + "'>");
+            document.getElementById("cel-c " + (i + 1) + " -l " + (j + 1)).innerHTML += ("<img src='img/wfairy.PNG' id='" + (i+10*j) + "'>");
         }
+		document.getElementById("cel-c " + (i + 1) + " -l " + (j + 1)).addEventListener("mousedown", clickdown);
+		document.getElementById("cel-c " + (i + 1) + " -l " + (j + 1)).addEventListener("mouseup", clickup);
     }
+}
+
+/*function deplacement(int x,int y){
+	
+}*/
+var i=true;
+var coordonnée_p1;
+var coordonnée_p2;
+
+function clickdown(){
+	if(i==true){
+		//coordonnée_p1=[];
+		var result = this.id.split(" ");
+		coordonnée_p1=[result[1],result[3]]
+		console.log(coordonnée_p1);
+		console.log("Veuilliez sélectionner une autre case pour effectuer le déplacement");
+	}
+	if(i==false){
+		var result = this.id.split(" ");
+		coordonnée_p2=[result[1],result[3]]
+		console.log(coordonnée_p2);
+		//console.log(result);
+		console.log("Déplacement effectué");
+	}
+	i=!i;
+	console.log(this.id);
+	this.style.backgroundColor = "red";
+	//i++;
+	console.log(i);
+}
+
+function clickup(){
+	console.log("ok");
+}
+
+function myScript(){
+	console.log("click");
 }
